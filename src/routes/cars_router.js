914d9d93c98e1e8c998a505e02980c511a0ade3a
Luiso <luisangelrochebroche@gmail.com>
2019-12-19 12:53:59 -0300

    se arreglo la ruta autos, aun falta la conexion a la base de datos

--- src/routes/cars_router.js
+++ src/routes/cars_router.js
@@ -1,6 +1,8 @@
-const expess  = require('events')
+const expess  = require('express')
 const router = expess.Router()
 
 const {getCars} = require('../controllers/cars_controllers')
 
 router.get('/',getCars)
+
+module.exports = router
