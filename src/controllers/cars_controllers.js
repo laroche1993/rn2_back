a1fad37fb5989e811d8d2e45954b0edab9fa09d3
Luiso <luisangelrochebroche@gmail.com>
2019-12-26 23:06:23 -0300

    Se termino la ruta autos con la conexion a la base de datos

--- src/controllers/cars_controllers.js
+++ src/controllers/cars_controllers.js
@@ -1,6 +1,33 @@
+const {Pool} = require('pg')
+
+const pool = new Pool({
+    host:process.env.PG_HOST,
+    user:process.env.PG_USER,
+    password:process.env.PG_PASSWORD,
+    database:process.env.PG_DATABASE,
+    port:process.env.PG_PORT
+})
+
+const query  = "SELECT autos.id,autos.created_at,autos.updated_at,autos.capacidadpasajero,autos.kmventas,autos.aire,coloresautos.nombrecolorauto,versionesautos.nombreversionauto,versionesautos.anno,versionesautos.cantidadpuerta,marcasautos.nombremarca,modelosautos.nombremodeloauto,nivelescombustiblesautos.nivelcombustiblevalor FROM public.autos JOIN public.coloresautos ON (autos.colorauto_id = coloresautos.id) JOIN versionesautos ON (autos.versionauto_id=versionesautos.id) JOIN public.modelosautos ON (versionesautos.modeloauto_id=modelosautos.id) LEFT JOIN public.nivelescombustiblesautos ON (autos.nivelcombustiblellegada_id=nivelescombustiblesautos.id) JOIN marcasautos ON(modelosautos.marcaauto_id=marcasautos.id)"
+const queryById  = "SELECT autos.id,autos.created_at,autos.updated_at,autos.capacidadpasajero,autos.kmventas,autos.aire,coloresautos.nombrecolorauto,versionesautos.nombreversionauto,versionesautos.anno,versionesautos.cantidadpuerta,marcasautos.nombremarca,modelosautos.nombremodeloauto,nivelescombustiblesautos.nivelcombustiblevalor FROM public.autos JOIN public.coloresautos ON (autos.colorauto_id = coloresautos.id) JOIN versionesautos ON (autos.versionauto_id=versionesautos.id) JOIN public.modelosautos ON (versionesautos.modeloauto_id=modelosautos.id) LEFT JOIN public.nivelescombustiblesautos ON (autos.nivelcombustiblellegada_id=nivelescombustiblesautos.id) JOIN marcasautos ON(modelosautos.marcaauto_id=marcasautos.id)"
+
 const Cars = {
-    getCars: (req, res) => {
-         res.send("aki va la info de los carros")
+    getCars: async (req, res) => {
+        try {        
+            const estudiante =  await pool.query(query);
+            res.json(estudiante.rows)
+         } catch (error) {
+             console.log(error)
+         }
+    },
+
+    getCarsById:async (req,res)=>{
+        try {
+            const estudiante =  await pool.query(queryById +'WHERE autos.id ='+req.params.id);
+            res.json(estudiante.rows)
+        } catch (error) {
+            
+        }
     }
 }
 module.exports = Cars
--- src/routes/cars_router.js
+++ src/routes/cars_router.js
@@ -1,8 +1,9 @@
 const expess  = require('express')
 const router = expess.Router()
 
-const {getCars} = require('../controllers/cars_controllers')
+const {getCars,getCarsById} = require('../controllers/cars_controllers')
 
 router.get('/',getCars)
+router.get('/:id',getCarsById)
 
 module.exports = router
